8ffd823afc5ce6decd025fb89f7ceba44744f3bb
Dasha1991Shilova <darjushka1@yandex.ru>
2023-11-25 22:25:12 +0400

    Create skript1.py

---
+++ skript1.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import matplotlib.pylot as plt
+
+pd.options.display.max_rows=2
+df = pd.read_csv('data.csv')
+strr = df.to_string()
+print(strr)
+print(df.head(1))
+print(df.tail(2))
+print("Информация о данных:")
+print(df.info())
+
+numeric_summary=df.describe()
+print("Простой анализ числовых данных:")
+print(numeric_summary)
+
+df['numeric_column'].hist(bins=20)
+plt.title('Гистограмма распределени данных')
+plt.xlabel('Значения')
+plt.ylabel('Частота')
+plt.show()
+
+plt.scatter(df['column1'], df['column2']
+plt.title('График зависимости между column1 и column2')
+plt.xlabel('column1')
+plt.ylabel('column2')
+plt.show()
